07cba4c6c3cdfd7eef61e24cde77162d89525a23
Jamesooni <kkangsoo@gmail.com>
2022-12-21 12:52:00 +0900

    edit style

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,21 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 
 module.exports = {
-   entry: "./src/client/js/main.js",
-   mode: "development",
-   output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, "assets", "js"),
+    entry: "./src/client/js/main.js",
+    mode: "development",
+    watch: true,
+    plugins: [
+        new MiniCssExtractPlugin({
+        filename: "css/styles.css",
+        }),
+    ],
+    output: {
+        filename: "js/main.js",
+        path: path.resolve(__dirname, "assets"),
+        clean: true,
    },
-   module: {
+    module: {
         rules: [
         {
             test: /\.js$/,
@@ -17,6 +25,10 @@ module.exports = {
                     presets: ["@babel/preset-env", { targets: "defaults" }],
                 },
             },
+        },
+        {
+            test: /\.scss$/,
+            use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
         },
         ],
     },
