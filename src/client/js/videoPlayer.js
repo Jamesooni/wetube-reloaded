7991b2e4756ab93b27fb2c75261f0dc0547e7c73
Jamesooni <kkangsoo@gmail.com>
2022-12-26 08:28:34 +0900

    Upgarade videoplayer

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,8 +1,12 @@
 const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
-const time = document.getElementById("time");
-const volume = document.getElementById("volume");
+const volumeRange = document.getElementById("volume");
+const currenTime = document.getElementById("currenTime");
+const totalTime = document.getElementById("totalTime");
+
+let volumeValue = 0.5;
+video.volume = volumeValue;
 
 const handlePlayClick = (e) => {
     if (video.paused) {
@@ -10,13 +14,41 @@ const handlePlayClick = (e) => {
     } else {
         video.pause();
     }
+    playBtn.innerText = video.paused ? "Play" : "Pause";
 };
-const handlePause = () => (playBtn.innerText = "Play");
-const handlePlay = () => (playBtn.innerText = "Pause");
 
-const handleMute = (e) => {};
+const handleMuteClick = (e) => {
+    if (video.muted) {
+      video.muted = false;
+    } else {
+      video.muted = true;
+    }
+    muteBtn.innerText = video.muted ? "Unmute" : "Mute";
+    volumeRange.value = video.muted ? 0 : volumeValue;
+};
+ 
+  const handleVolumeChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    if (video.muted) {
+      video.muted = false;
+      muteBtn.innerText = "Mute";
+    }
+    volumeValue = value;
+    video.volume = value;
+};
+
+const handleLoadedMetadata = () => {
+    totalTime.innerText = Math.floor(video.duration);
+};
+ 
+const handleTimeUpdate = () => {
+    currenTime.innerText = Math.floor(video.currentTime);
+};
 
 playBtn.addEventListener("click", handlePlayClick);
-muteBtn.addEventListener("click", handleMute);
-video.addEventListener("pause", handlePause);
-video.addEventListener("play", handlePlay);
+muteBtn.addEventListener("click", handleMuteClick);
+volumeRange.addEventListener("input", handleVolumeChange);
+video.addEventListener("loadedmetadata", handleLoadedMetadata);
+video.addEventListener("timeupdate", handleTimeUpdate);
